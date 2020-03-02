1aacc8b5f5c83bc8cd095559c68e311d39fb3b8f
Damango <37273048+Damango@users.noreply.github.com>
2020-03-01 20:20:51 -0500

    Add files via upload

--- src/passwordGenerator/Controller.java
+++ src/passwordGenerator/Controller.java
@@ -22,11 +22,10 @@ public class Controller {
 	
 	private static String[] lowerCaseCharacters = {"a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z",};
 	private static String[] upperCaseCharacters = {"A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"};
-	private static String[] theNumbers = {"1","2","3","4","5","6","7","8","9","0"};
-	private static String[] symbols = {"!","@","#","$","%","^","&","*"};
+	private static String[] theNumbers = {"1","2","3","4","5","6","7","8","9","0", "1","2","3","4","5","6","7","8","9","0"};
+	private static String[] symbols = {"!","@","#","$","%","^","&","*","!","@","#","$","%","^","&","*"};
 	
-	private  String[] testArray1 = {"1","2", "3"};
-	private  String[] testArray2 = {"4","5", "6"};
+	
 	
 	private double test;
 
